39d957e0896f04daf124df485b08142d02f8af27
paulocheque <paulocheque@gmail.com>
2013-04-21 21:24:41 -0300

    [config] Initial project configuration

---
+++ setup.py
@@ -0,0 +1,36 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+VERSION = open('VERSION', 'r').read().strip()
+PROJECT_NAME = 'gat-python'
+
+tests_require = [
+    'nose',
+    'coverage',
+]
+
+install_requires = [
+    'six==1.3.0',
+    'simplejson==3.1.3',
+    'pyzmq==13.0.2',
+]
+
+setup(name='%s' % PROJECT_NAME,
+      url='https://github.com/gatournament/%s' % PROJECT_NAME,
+      author="gatournament",
+      author_email='contact@gatournament.com',
+      keywords='',
+      description='',
+      license='MIT',
+      classifiers=[
+          'Operating System :: OS Independent',
+          'Topic :: Software Development'
+      ],
+
+      version='%s' % VERSION,
+      install_requires=install_requires,
+      tests_require=tests_require,
+
+      packages=find_packages(),
+)
+
