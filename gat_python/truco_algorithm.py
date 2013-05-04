28a302deaeb4a06db2a0438b6c092e137007fa1e
paulocheque <paulocheque@gmail.com>
2013-05-03 21:07:40 -0300

    [refactoring][truco] accept_or_giveup_truco renamed to accept_truco

--- gat_python/truco_algorithm.py
+++ gat_python/truco_algorithm.py
@@ -6,8 +6,8 @@ class TrucoAlgorithm(GameAlgorithm):
     def process_message(self, message):
         if message['action'] == 'play':
             self.play(message['context'])
-        elif message['action'] == 'accept_or_giveup_truco':
-            accept = self.accept_or_giveup_truco(message['context'])
+        elif message['action'] == 'accept_truco':
+            accept = self.accept_truco(message['context'])
             if accept:
                 self.send_response({'action': 'accept_truco'})
             else:
@@ -16,11 +16,11 @@ class TrucoAlgorithm(GameAlgorithm):
     def play(self, context):
         pass
 
-    def accept_or_giveup_truco(self, context):
+    def accept_truco(self, context):
         pass
 
-    def upcard(self, index):
-        self.send_response({'action': 'upcard', 'hand_index': index})
+    def upcard(self, card):
+        self.send_response({'action': 'upcard', 'hand_card': card})
 
     def truco(self):
         self.send_response({'action': 'truco'})
--- tests/test_truco_algorithm.py
+++ tests/test_truco_algorithm.py
@@ -12,7 +12,7 @@ class StubTrucoAlgorithm(TrucoAlgorithm):
     def play(self, context):
         self.execute_play_method = True
 
-    def accept_or_giveup_truco(self, context):
+    def accept_truco(self, context):
         self.execute_accept_method = True
 
     def send_response(self, message):
@@ -28,6 +28,6 @@ class TrucoAlgorithmTests(unittest.TestCase):
 
     def test_accept_or_giveup_truco_message(self):
         algorithm = StubTrucoAlgorithm()
-        algorithm.process_message({'action': 'accept_or_giveup_truco', 'context':{}})
+        algorithm.process_message({'action': 'accept_truco', 'context':{}})
         self.assertEquals(False, algorithm.execute_play_method)
         self.assertEquals(True, algorithm.execute_accept_method)
